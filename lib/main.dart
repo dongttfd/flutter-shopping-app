b8065c10b8d3de416cb480ed362db5aaa86df7e5
dongtt <dongtt.fd@gmail.com>
2021-04-24 02:34:30 +0700

    #188 product overview init

---
+++ lib/main.dart
@@ -0,0 +1,32 @@
+import 'package:flutter/material.dart';
+
+import 'package:shopping/screens/products_overview_screen.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'My Shop',
+      theme: ThemeData(
+        primarySwatch: Colors.teal,
+      ),
+      home: ProductsOverviewScreen(),
+    );
+  }
+}
+
+class MyHomePage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('My Shop'),
+      ),
+      body: Center(
+        child: Text('Home'),
+      ),
+    );
+  }
+}
